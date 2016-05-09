bc8df435e6f125db94290086553f55ce320aee21
Marco Silva <marcowilliamdf@gmail.com>
2016-05-09 02:39:53 -0300

    adding project file

---
+++ optando.pl
@@ -0,0 +1,285 @@
+:-op(900, xfx, [temFoco, ehDaArea, temDificuldade]).
+
+'calculo 3' ehDaArea matematica.
+'ciencias aeroespaciais' ehDaArea aeroespacial.
+'circuitos eletronicos 1' ehDaArea eletronica.
+'combustiveis e biocombustiveis' ehDaArea energia.
+'computacao em nuvem' ehDaArea computacao.
+'confiabilidade de sistemas' ehDaArea computacao.
+'contratacao de TI' ehDaArea software.
+'criptografia e seguranca de redes' ehDaArea computacao.
+'desenvolvimento sustentavel' ehDaArea energia.
+'design automotivo' ehDaArea automotiva.
+'design de sistemas aeroespaciais' ehDaArea aeroespacial.
+'elementos e metodos em eletronica' ehDaArea eletronica.
+'eletricidade aplicada' ehDaArea matematica.
+'eletronica veicular' ehDaArea automotiva.
+'engenharia de seguranca do trabalho' ehDaArea engenharia.
+'engenharia de software experimental' ehDaArea cientifica.
+'equacoes diferenciais 1' ehDaArea matematica.
+'ergonomia do produto' ehDaArea automotiva.
+'estruturas de dados e algoritimos 2' ehDaArea programacao.
+'fenomenos de transporte' ehDaArea engenharia.
+'fisica moderna' ehDaArea matematica.
+'fisica para jogos eletronicos 1' ehDaArea jogos.
+'fontes energeticas e conversoes' ehDaArea energia.
+'eletromag' ehDaArea matematica.
+'fundamentos de qualidade de software' ehDaArea qualidade.
+'fundamentos de sistemas distribuidos' ehDaArea eletronica.
+'fundamentos de compiladores para engenharia' ehDaArea geral.
+'fundamentos de arquitetura de software' ehDaArea software.
+'fundamentos de compiladores' ehDaArea computacao.
+'fundamentos de redes de computadores'ehDaArea computacao.
+'fundamentos de sistemas operacionais' ehDaArea computacao.
+'gestao ambiental para engenharia' ehDaArea engenharia.
+'gestao da producao automotiva' ehDaArea automotiva.
+'instrumentacao de sistemas de controle' ehDaArea eletronica.
+'introducao a atividade empresarial' ehDaArea geral.
+'introducao a computacao grafica' ehDaArea computacao.
+'introducao a jogos eletronicos' ehDaArea jogos.
+'introducao ao desenvolvimento de jogos' ehDaArea jogos.
+'introducao a engenharia' ehDaArea engenharia.
+'laboratorio de bando de dados' ehDaArea computacao.
+'materias de construcao de engenharia' ehDaArea engenharia.
+'matematica aplicada a sistemas' ehDaArea matematica.
+'mecanica do voo' ehDaArea aeroespacial.
+'mecanicas dos solidos' ehDaArea engenharia.
+'metodos experimentias para engenharia' ehDaArea engenharia.
+'metodologia da escrita cientifica' ehDaArea cientifica.
+'metodos matematicos para engenharia' ehDaArea engenharia.
+'microprocessadores e microcontroladores' ehDaArea eletronica.
+'modelagem de processo' ehDaArea software.
+'paradigmas de programacao' ehDaArea programacao.
+'planejamento da gestao de energia' ehDaArea energia.
+'pre calculo' ehDaArea engenharia.
+'principios de robotica educacional' ehDaArea eletronica.
+'principios de computacao grafica' ehDaArea computacao.
+'processo de desenvolvimento de software' ehDaArea software.
+'processos petroquimicos' ehDaArea energia.
+'programacao para competicoes' ehDaArea programacao.
+'programacao web' ehDaArea programacao.
+'projeto de circuitos integrados' ehDaArea eletronica.
+'projeto de veiculos' ehDaArea automotiva.
+'projeto de sistemas de controle' ehDaArea automotiva.
+'seminarios em engenharia de software' ehDaArea software.
+'sistemas criticos tolerantes a falhas' ehDaArea computacao.
+'sistemas aeroespaciais' ehDaArea aeroespacial.
+'sistemas automotivos' ehDaArea automotiva.
+'sistemas de propulsao hibridos' ehDaArea automotiva.
+'sistemas digitais 2' ehDaArea eletronica.
+'sistemas embarcados' ehDaArea eletronica.
+'tecnicas de medicao e estimativas em software' ehDaArea software.
+'topicos especiais de engenharia de software' ehDaArea software.
+'topicos especiais em fisica aplicada' ehDaArea matematica.
+'topicos especiais em governanca de TI' ehDaArea software.
+'topicos especiais em jogos digitais' ehDaArea jogos.
+'topicos especiais em matematica aplicada' ehDaArea matematica.
+'topicos especiais em sistema criticos' ehDaArea eletronica.
+'topicos especiais em programaca' ehDaArea programacao.
+
+
+'calculo 3'temFoco teorica.
+'ciencias aeroespaciais' temFoco teorica.
+'circuitos eletronicos 1' temFoco hibrida.
+'combustiveis e biocombustiveis' temFoco teorica.
+'computacao em nuvem' temFoco teorica.
+'confiabilidade de sistemas' temFoco teorica.
+'contratacao de TI' temFoco teorica.
+'criptografia e seguranca de redes'temFoco hibrida.
+'desenvolvimento sustentavel' temFoco hibrida.
+'design automotivo' temFoco hibrida.
+'design de sistemas aeroespaciais' temFoco pratica.
+'elementos e metodos em eletronica' temFoco teorica.
+'eletricidade aplicada' temFoco teorica.
+'eletronica veicular' temFoco teorica.
+'engenharia de seguranca do trabalho' temFoco teorica.
+'engenharia de software experimental' temFoco hibrida.
+'equacoes diferenciais 1' temFoco teorica.
+'ergonomia do produto' temFoco pratica.
+'estruturas de dados e algoritimos 2' temFoco teorica.
+'fenomemos de transporte' temFoco teorica.
+'fisica moderna' temFoco teorica.
+'fisica para jogos eletronicos 1' temFoco hibrida.
+'fontes energeticas e conversoes' temFoco pratica.
+'eletromag' temFoco teorica.
+'fundamentos de qualidade de software' temFoco teorica.
+'fundamentos de sistemas distribuidos' temFoco teorica.
+'fundamentos de compiladores para engenharia' temFoco hibrida.
+'fundamentos de arquitetura de software' temFoco teorica.
+'fundamentos de compiladores' temFoco pratica.
+'fundamentos de redes de computadores'temFoco hibrida.
+'fundamentos de sistemas operacionais' temFoco hibrida.
+'gestao ambiental para engenharia' temFoco pratica.
+'gestao da producao automotiva' temFoco pratica.
+'instrumentacao de sistemas de controle' temFoco teorica.
+'introducao a atividade empresarial' temFoco pratica.
+'introducao a computacao grafica' temFoco hibrida.
+'introducao a jogos eletronicos' temFoco hibrida.
+'introducao ao desenvolvimento de jogos' temFoco pratica.
+'introducao a engenharia' temFoco pratica.
+'laboratorio de bando de dados' temFoco pratica.
+'materias de construcao de engenharia' temFoco teorica.
+'matematica aplicada a sistemas' temFoco teorica.
+'mecanica do voo' temFoco teorica.
+'mecanicas dos solidos' temFoco teorica.
+'metodos experimentais para engenharia' temFoco pratica.
+'metodologia da escrita cientifica' temFoco pratica.
+'metodos matematicos para engenharia' temFoco teorica.
+'microprocessadores e microcontroladores' temFoco hibrida.
+'modelagem de processo' temFoco hibrida.
+'paradigmas de programacao' temFoco hibrida.
+'planejamento da gestao de energia' temFoco hibrida.
+'pre calculo' temFoco teorica.
+'principios de robotica educacional' temFoco teorica.
+'principios de computacao grafica' temFoco pratica.
+'processo de desenvolvimento de software' temFoco teorica.
+'processos petroquimicos' temFoco teorica.
+'programacao para competicoes' temFoco hibrida.
+'programacao web' temFoco hibrida.
+'projeto de circuitos integrados' temFoco hibrida.
+'projeto de veiculos' temFoco hibrida.
+'projeto de sistemas de controle' temFoco hibrida.
+'seminarios em engenharia de software' temFoco pratica.
+'sistemas criticos tolerantes a falhas' temFoco teorica.
+'sistemas aeroespaciais' temFoco teorica.
+'sistemas automotivos' temFoco teorica.
+'sistemas de propulsao hibridos' temFoco teorica.
+'sistemas digitais 2' temFoco hibrida.
+'sistemas embarcados' temFoco hibrida.
+'tecnicas de medicao e estimativas em software' temFoco teorica.
+'topicos especiais de engenharia de software' temFoco pratica.
+'topicos especiais em fisica aplicada' temFoco pratica.
+'topicos especiais em governanca de TI' temFoco pratica.
+'topicos especiais em jogos digitais' temFoco pratica.
+'topicos especiais em matematica aplicada' temFoco pratica.
+'topicos especiais em sistema criticos' temFoco pratica.
+'topicos especiais em programaca' temFoco pratica.
+
+
+'calculo 3'temDificuldade dificil.
+'ciencias aeroespaciais' temDificuldade media.
+'circuitos eletronicos 1' temDificuldade media.
+'combustiveis e biocombustiveis' temDificuldade media.
+'computacao em nuvem' temDificuldade facil.
+'confiabilidade de sistemas' temDificuldade media.
+'contratacao de TI' temDificuldade media.
+'criptografia e seguranca de redes'temDificuldade media.
+'desenvolvimento sustentavel' temDificuldade media.
+'design automotivo' temDificuldade media.
+'design de sistemas aeroespaciais' temDificuldade media.
+'elementos e metodos em eletronica' temDificuldade facil.
+'eletricidade aplicada' temDificuldade dificil.
+'eletronica veicular' temDificuldade media.
+'engenharia de seguranca do trabalho' temDificuldade facil.
+'engenharia de software experimental' temDificuldade media.
+'equacoes diferenciais 1' temDificuldade dificil.
+'ergonomia do produto' temDificuldade facil.
+'estruturas de dados e algoritmos 2' temDificuldade dificil.
+'fenomemos de transporte' temDificuldade media.
+'fisica moderna' temDificuldade dificil.
+'fisica para jogos eletronicos 1' temDificuldade media.
+'fontes energeticas e conversoes' temDificuldade media.
+'eletromag' temDificuldade dificil.
+'fundamentos de qualidade de software' temDificuldade media.
+'fundamentos de sistemas distribuidos' temDificuldade media.
+'fundamentos de compiladores para engenharia' temDificuldade media.
+'fundamentos de arquitetura de software' temDificuldade media.
+'fundamentos de compiladores' temDificuldade media.
+'fundamentos de redes de computadores'temDificuldade media.
+'fundamentos de sistemas operacionais' temDificuldade dificil.
+'gestao ambiental para engenharia' temDificuldade media.
+'gestao da producao automotiva' temDificuldade media.
+'instrumentacao de sistemas de controle' temDificuldade media.
+'introducao a atividade empresarial' temDificuldade facil.
+'introducao a computacao grafica' temDificuldade media.
+'introducao a jogos eletronicos' temDificuldade media.
+'introducao ao desenvolvimento de jogos' temDificuldade media.
+'introducao a engenharia' temDificuldade facil.
+'laboratorio de bando de dados' temDificuldade media.
+'materias de construcao de engenharia' temDificuldade facil.
+'matematica aplicada a sistemas' temDificuldade media.
+'mecanica do voo' temDificuldade media.
+'mecanicas dos solidos' temDificuldade media.
+'metodos experimentais para engenharia' temDificuldade media.
+'metodologia da escrita cientifica' temDificuldade media.
+'metodos matematicos para engenharia' temDificuldade media.
+'microprocessadores e microcontroladores' temDificuldade media.
+'modelagem de processo' temDificuldade media.
+'paradigmas de programacao' temDificuldade media.
+'planejamento da gestao de energia' temDificuldade media.
+'pre calculo' temDificuldade facil.
+'principios de robotica educacional' temDificuldade facil.
+'principios de computacao grafica' temDificuldade dificil.
+'processo de desenvolvimento de software' temDificuldade facil.
+'processos petroquimicos' temDificuldade media.
+'programacao para competicoes' temDificuldade dificil.
+'programacao web' temDificuldade media.
+'projeto de circuitos integrados' temDificuldade media.
+'projeto de veiculos' temDificuldade media.
+'projeto de sistemas de controle' temDificuldade media.
+'seminarios em engenharia de software' temDificuldade media.
+'sistemas criticos tolerantes a falhas' temDificuldade media.
+'sistemas aeroespaciais' temDificuldade media.
+'sistemas automotivos' temDificuldade media.
+'sistemas de propulsao hibridos' temDificuldade media.
+'sistemas digitais 2' temDificuldade dificil.
+'sistemas embarcados' temDificuldade media.
+'tecnicas em medicao e estimativas em software' temDificuldade facil.
+'topicos especiais de engenharia de software' temDificuldade media.
+'topicos especiais em fisica aplicada' temDificuldade media.
+'topicos especiais em governanca de TI' temDificuldade facil.
+'topicos especiais em jogos digitais' temDificuldade media.
+'topicos especiais em matematica aplicada' temDificuldade media.
+'topicos especiais em sistema criticos' temDificuldade dificil.
+'topicos especiais em programaca' temDificuldade dificil.
+
+menu:- repeat,nl,nl,nl,
+	tab(30),write('=== OPTANDO ==='), nl,
+	tab(15),write('Responda as perguntas escrevendo a opcao desejada'), nl,
+	tab(15),write('e saiba quais materias encaixam com o seu perfil'), nl,
+	write('1. Iniciar'), nl,
+	write('0. Sair'), nl,
+	read(X),
+	option(X),
+	X==0,
+	!.
+
+option(0):- !.
+option(1):- perguntas.
+
+perguntas:- 
+	tab(10),write("Escreva uma opcao de area assim como escrito a baixo"),nl,
+	write("matematica"),nl,
+	write("aeroespacial"),nl,
+	write("eletronica"),nl,
+	write("energia"),nl,
+	write("automotiva"),nl,
+	write("software"),nl,
+	write("engenharia"),nl,
+	write("programacao"),nl,
+	write("computacao"),nl,
+	write("cientifica"),nl,
+	write("jogos"),nl,
+	write("geral"),nl,
+	read(Y),
+	perguntas2(Y).
+
+perguntas2(Y):-
+	tab(15),write("Escreva a dificuldade assim como escrito abaixo"),nl,
+	write("dificil"),nl,
+	write("media"),nl,
+	write("facil"),nl,
+	read(Z),
+	perguntas3(Y,Z).
+
+perguntas3(Y,Z):-
+	tab(20),write("Escreva o foco assim como escrito abaixo"),nl,
+	write("teorica"),nl,
+	write("pratica"),nl,
+	write("hibrida"),nl,
+	read(K),
+	resultado(Y,Z,K).
+
+resultado(Y,Z,K):- 
+	X temFoco K, X ehDaArea Y, X temDificuldade Z, nl , write("-->"), write(X),nl.
+	
